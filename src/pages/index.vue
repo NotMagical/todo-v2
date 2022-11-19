8c35ad0a85d641b121dcc067d858d8aa080cf452
NotMagical <NotMagicalDev@gmail.com>
2022-11-19 02:39:17 +0000

    feat: todo base.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-const name = $ref('')
+let text = $ref('')
+let editText = $ref('')
+const todos: string[] = $ref([])
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+// const router = useRouter()
+const addTodo = () => {
+  if (!text)
+    return
+
+  todos.push(text)
+  text = ''
+  // if (text)
+  //   router.push(`/hi/${encodeURIComponent(text)}`)
+}
+const updateTodo = (idx: number) => {
+  todos[idx] = editText
+  editText = ''
+}
+const removeTodo = (idx: number) => {
+  todos.splice(idx, 1)
 }
 </script>
 
@@ -13,38 +27,73 @@ const go = () => {
     <div i-carbon-campsite text-4xl inline-block />
     <p>
       <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
+        Todo - v2
       </a>
     </p>
     <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
+      <em text-sm op75>Todos - Counters - Trackers</em>
     </p>
 
     <div py-4 />
 
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
     <div>
+      <input
+        id="input"
+        v-model="text"
+        placeholder="What's on your mind?"
+        type="text"
+        autocomplete="false"
+        p="x-4 y-2"
+        w="250px"
+        text="center"
+        bg="transparent"
+        border="~ rounded gray-200 dark:gray-700"
+        outline="none active:none"
+        @keydown.enter="addTodo"
+      >
       <button
         class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
+        :disabled="!text"
+        @click="addTodo"
       >
-        Go
+        Add
       </button>
+    </div>
+
+    <div class="flex flex-col items-center">
+      <div
+        v-for="(i, idx) in todos" :key="idx"
+        class=" dark:bg-gray-800 w-2/6 flex flex-row gap-x-5 border border-color-gray-800 dark:border-color-gray-700 py-sm rounded my-xs px-sm justify-center items-center"
+      >
+        <div>
+          {{ idx + 1 }}
+        </div>
+        <div class="w-5/6">
+          <div v-if="!editText.length" @click="editText = i">
+            {{ i }}
+          </div>
+          <div v-else>
+            <input
+              id="input"
+              v-model="editText"
+              placeholder="What's on your mind?"
+              type="text"
+              autocomplete="false"
+              p="x-4 y-2"
+              w="250px"
+              text="center"
+              bg="transparent"
+              border=" bottom"
+              outline="none active:none"
+              @keydown.enter="updateTodo(idx)"
+            >
+          </div>
+        </div>
+        <div class="flex flex-row items-center gap-5">
+          <div v-if="editText.length" class="i-carbon-edit bg-green cursor-pointer hover:bg-green-700" @click="updateTodo(idx)" />
+          <div class="i-carbon:trash-can bg-red cursor-pointer hover:bg-red-700" @click="removeTodo(idx)" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
